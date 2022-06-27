f5aa855cf96917752f7b22e47cd6c35f3b518997
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-27 11:55:48 +0600

    navbar added

---
+++ components/utils/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="navbar bg-sky-400">
+                <div className="flex-1">
+                    <a className="text-xl normal-case btn btn-ghost">IMFD</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="p-0 menu menu-horizontal">
+                        <li>
+                            <Link href="/company-info" passHref>
+                                <a>Company Info</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
